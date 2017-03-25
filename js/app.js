7f9236ded23f0ec1e33c36f79299a63f28863385
trevans24 <trevans24@gmail.com>
2017-03-25 10:14:54 -0600

    added players to gameboard, made game board background image, added JS for player var & function to determine pixel movement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+// menu click animation
 
-function myFunction(x){
+var myFunction = function(x){
 	x.classList.toggle('change');
+};
+
+
+$(document).ready(function(){
+	console.log("up and running!");
+//defining players
+var playerOne = document.getElementsByClassName('playerOne'),
+	playerTwo = document.getElementsByClassName('playerTwo');
+
+//distance to move player per key press
+function distance(min,max){
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.Random()*(max - min)) + min;
 }
+
+//moving the player per the distance determined
+function movePlayer(){
+
+}
+
+
+//keypress event listeners
+playerOne.addEventListener("keydown", movePlayer());
+playerTwo.addEventListener("keydown", movePlayer());
+
+
+//finish line
+
+
+//reset game
+});
