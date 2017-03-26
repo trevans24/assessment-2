fbb9ddd5c9700e7094732f0c0172d75277d36338
trevans24 <trevans24@gmail.com>
2017-03-25 21:59:08 -0600

    changing js, redo...

--- js/app.js
+++ js/app.js
@@ -4,37 +4,47 @@ var myFunction = function(x){
 	x.classList.toggle('change');
 };
 
-
 $(document).ready(function(){
-	console.log("up and running!");
-//defining players
-var playerOne = document.getElementsByClassName('playerOne'),
-	playerTwo = document.getElementsByClassName('playerTwo');
 
+console.log("up and running!");
+
+$('.move').click(function(){
+	$('playerOne').animate({
+	marginLeft: "+=50",
+});
+});
+
+
+
+
+});
 //distance to move player per key press
-function distance(min,max){
-	console.log("1");
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	console.log("2");
-	return Math.floor(Math.Random()*(max - min)) + min;
+// function distance(min,max){
+// 	console.log("1");
+// 	min = Math.ceil(min);
+// 	max = Math.floor(max);
+// 	console.log("2");
+// 	return Math.floor(Math.Random()*(max - min)) + min;
 	
-}
+// }
 
 //moving the player per the distance determined
-function movePlayer(){
-	console.log("3");
-	(parsInt(document.getElementsByClassName('playerOne').style.right = right + distance(2,15)));
-	console.log("4");
-}
-
-
-//keypress event listeners
-playerOne.addEventListener("keydown", movePlayer());
-playerTwo.addEventListener("keydown", movePlayer());
+// function movePlayer(e){
+// 	console.log("3");
+// 	if (e.keycode == 65){
+// 		moveRight += distance(2,15);
+// 		playerOne.style.left = left + "px";
+// 		console.log("4");
+// 	} if (e.keycode === 76){
+// 		moveRight += distance(2,15);
+// 		playerTwo.style.left = left + "px";
+// 		console.log("5");
+// 	}
+// 	console.log("6");
+// }
 
 //finish line
 
 
 //reset game
-});
+
