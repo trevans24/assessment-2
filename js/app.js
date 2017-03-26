0f119664bfddaa2475cf8976ad848a86104ce717
trevans24 <trevans24@gmail.com>
2017-03-25 19:24:47 -0600

    added styling to player divs and changes to move function

--- js/app.js
+++ js/app.js
@@ -13,21 +13,25 @@ var playerOne = document.getElementsByClassName('playerOne'),
 
 //distance to move player per key press
 function distance(min,max){
+	console.log("1");
 	min = Math.ceil(min);
 	max = Math.floor(max);
+	console.log("2");
 	return Math.floor(Math.Random()*(max - min)) + min;
+	
 }
 
 //moving the player per the distance determined
 function movePlayer(){
-
+	console.log("3");
+	(parsInt(document.getElementsByClassName('playerOne').style.right = right + distance(2,15)));
+	console.log("4");
 }
 
 
 //keypress event listeners
 playerOne.addEventListener("keydown", movePlayer());
 playerTwo.addEventListener("keydown", movePlayer());
-
 
 //finish line
 
